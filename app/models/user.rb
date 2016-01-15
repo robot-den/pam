bc6fefa8e36c27f6408ade39006743c29cb1bc77
robot-den <w.nazmutdinov@gmail.com>
2016-01-15 23:56:01 +0500

    added and conf user model for devise, added gem omniauth

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+
+  # Include default devise modules. Others available are: :confirmable, :lockable and :timeoutable
+  devise  :database_authenticatable, 
+          :registerable, 
+          :recoverable, 
+          :rememberable, 
+          :trackable, 
+          :validatable, 
+          :omniauthable
+end
