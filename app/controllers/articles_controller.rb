8fc29d3e7202fbb61562eaa415fb03b1ef45b406
robot-den <w.nazmutdinov@gmail.com>
2016-01-17 15:36:57 +0500

    add actions and modify index view for article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,26 @@
 class ArticlesController < ApplicationController
+
   def index
-    
+    #FIXME
+    @articles = Article.all
   end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
