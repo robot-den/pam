c5562fa1c9598ef3da9e76433001a596c59fbcdb
robot-den <w.nazmutdinov@gmail.com>
2016-01-17 23:19:16 +0500

    include basic authentificate for articles by devise and name/email login

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,21 @@ class User < ActiveRecord::Base
 
   has_many :articles
 
+  attr_accessor :login
+
+  validates :name, :presence => true, :uniqueness => { :case_sensitive => false }
+  #FIXME need change regular
+  validates_format_of :name, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:name) || conditions.has_key?(:email)
+      conditions[:email].downcase! if conditions[:email]
+      where(conditions.to_hash).first
+    end
+  end
+
 end
