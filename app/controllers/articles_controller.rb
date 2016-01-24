9992cc25cbb8e8730e2074094b8ce7d745548775
robot-den <w.nazmutdinov@gmail.com>
2016-01-25 00:12:22 +0500

    fix some bugs with saving comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -52,17 +52,18 @@ class ArticlesController < ApplicationController
 
     def take_sort_comments(article)
       a = []
-      if b = Comment.where("article_id = #{article.id}") then
+      b = Comment.where("article_id = #{article.id}")
+      unless b.empty?
         a = b.select { |x| x.commentable_type == 'Article' }
         b = b.to_a.reject! { |x| x.commentable_type == 'Article' }
-      end
-      while b.empty? == false
-        a.each_with_index do |elem, i|
-          d = b.select { |z| z.commentable_id == elem.id }
-          if d != []
-              a = a.insert(i+1, d).flatten
-              b.reject! { |z| z.commentable_id == elem.id }
-              break
+        while b.empty? == false
+          a.each_with_index do |elem, i|
+            d = b.select { |z| z.commentable_id == elem.id }
+            if d != []
+                a = a.insert(i+1, d).flatten
+                b.reject! { |z| z.commentable_id == elem.id }
+                break
+            end
           end
         end
       end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,20 +2,23 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
-    if params[:commentable_type] == 'Article' && Article.exist?(params[:article_id]) && params[:commentable_id] == params[:article_id]
+    binding.pry
+    if comment_params[:commentable_type] == 'Article' && Article.exists?(comment_params[:article_id]) && comment_params[:commentable_id] == comment_params[:article_id]
       if @comment.save
         redirect_to articles_path
       else
         redirect_to articles_path
       end
     end
-    if params[:commentable_type] == 'Comment' && Comment.exist?(params[:commentable_id]) && Comment.find(params[:commentable_id]).article_id == params[:article_id]
+    if comment_params[:commentable_type] == 'Comment' && Comment.exists?(comment_params[:commentable_id]) && Comment.find(comment_params[:commentable_id]).article_id == comment_params[:article_id]
       if @comment.save
         redirect_to articles_path
       else
         redirect_to articles_path
       end
     end
+
+    binding.pry
   end
 
   def destroy
