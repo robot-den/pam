56c4eb0e14e89bb504b9f38b05097523b9a18945
robot-den <w.nazmutdinov@gmail.com>
2016-02-22 17:56:24 +0500

    Fix trouble with redirect when login with Facebook

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,11 +2,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # You need to implement the method below in your model (e.g. app/models/user.rb)
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    binding.pry
 
     if @user.persisted?
-      sign_in_and_redirect @user
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      #FIXME look at adress in browser - "#_=_"
+      sign_in @user
+      redirect_to root_path
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
