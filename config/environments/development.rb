ff3437f09aa2ec0a9b7ae4d421e22198a001873e
robot-den <w.nazmutdinov@gmail.com>
2016-03-02 17:09:42 +0500

    variables moves to ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,16 +44,4 @@ Rails.application.configure do
   # }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  
-
-  # config.action_mailer.smtp_settings = {
-  #   address: "smtp.gmail.com",
-  #   port: 587,
-  #   domain: 'google.com',
-  #   authentication: "plain",
-  #   enable_starttls_auto: true,
-  #   user_name: 'peoplearoundmail@gmail.com',
-  #   #FIXME
-  #   password: '05010501'
-  # }
 end
