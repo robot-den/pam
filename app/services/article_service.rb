d6a7179a5b721ebee65f989ea98cce67e1757b1d
robot-den <w.nazmutdinov@gmail.com>
2016-03-02 23:03:24 +0500

    disable notification and search for heroku

--- app/services/article_service.rb
+++ app/services/article_service.rb
@@ -4,13 +4,15 @@ class ArticleService
     # save article with new attributes
     article.assign_attributes(params)
     article.save!
-    # send mail to article's author
-    UserMailer.status_article_notification(article.id).deliver_later unless article.approved.nil?
-    # send mails to subscribers of article's categories
-    if article.approved?
-      article.list_of_subscribers.each do |user_id|
-          UserMailer.subscribers_notification(article.id, user_id).deliver_later
-      end
-    end
+
+    # # NOTE: FOR HEROKU MAIL SENDING IS DISABLED
+    # # send mail to article's author
+    # UserMailer.status_article_notification(article.id).deliver_later unless article.approved.nil?
+    # # send mails to subscribers of article's categories
+    # if article.approved?
+    #   article.list_of_subscribers.each do |user_id|
+    #       UserMailer.subscribers_notification(article.id, user_id).deliver_later
+    #   end
+    # end
   end
 end
